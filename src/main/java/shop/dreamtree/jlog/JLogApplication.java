d5bced4194fe16d926d4d8b817390d669608cafd
Zeus6768 <akaps6768@gmail.com>
2024-02-16 21:00:22 +0900

    initial commit

---
+++ src/main/java/shop/dreamtree/jlog/JLogApplication.java
@@ -0,0 +1,13 @@
+package shop.dreamtree.jlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JLogApplication.class, args);
+    }
+
+}
