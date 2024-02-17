7fadb733376bc4087c8d600711763c9f066ab39b
Zeus6768 <akaps6768@gmail.com>
2024-02-17 14:52:37 +0900

    feat: 예외 처리 구현

--- src/main/java/shop/dreamtree/jlog/logs/LogsController.java
+++ src/main/java/shop/dreamtree/jlog/logs/LogsController.java
@@ -1,7 +1,11 @@
 package shop.dreamtree.jlog.logs;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -44,5 +48,15 @@ public class LogsController {
     String delete(@RequestBody LogsDto logsDto) {
         logsService.delete(logsDto);
         return "Log deleted";
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException exception) {
+        return ResponseEntity.badRequest().body("존재하지 않는 코드 또는 이름입니다.");
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body("접근할 수 없는 방입니다.");
     }
 }
