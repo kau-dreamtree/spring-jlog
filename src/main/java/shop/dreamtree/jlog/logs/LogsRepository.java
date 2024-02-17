417dc03717b68121289a4e23422b6229d059c096
Zeus6768 <akaps6768@gmail.com>
2024-02-17 13:45:09 +0900

    fix: 날짜 기준 내림차순으로 변경

--- src/main/java/shop/dreamtree/jlog/logs/LogsRepository.java
+++ src/main/java/shop/dreamtree/jlog/logs/LogsRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LogsRepository extends JpaRepository<Logs, Long> {
-    List<Logs> getAllByRoomUid(String uid);
+    List<Logs> getAllByRoomUidOrderByCreatedDateAsc(String uid);
 }
