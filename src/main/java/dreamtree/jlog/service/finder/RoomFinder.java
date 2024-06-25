f9cc487b49c29e48ebf79d01ac41f10aa5521502
Zeus6768 <akaps6768@gmail.com>
2024-06-25 17:47:50 +0900

    fix: reformat delete request

--- src/main/java/dreamtree/jlog/service/finder/RoomFinder.java
+++ src/main/java/dreamtree/jlog/service/finder/RoomFinder.java
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Service;
 
 import dreamtree.jlog.domain.Room;
 import dreamtree.jlog.exception.JLogException;
-import dreamtree.jlog.repository.RoomJpaRepository;
+import dreamtree.jlog.repository.RoomRepository;
 
 @Service
 public class RoomFinder {
 
-    private final RoomJpaRepository roomRepository;
+    private final RoomRepository roomRepository;
 
-    public RoomFinder(RoomJpaRepository roomRepository) {
+    public RoomFinder(RoomRepository roomRepository) {
         this.roomRepository = roomRepository;
     }
 
