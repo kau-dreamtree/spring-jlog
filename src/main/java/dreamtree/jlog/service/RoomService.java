5fb6653ec02c7301e3ab3a166591040fd4494ad5
Zeus6768 <akaps6768@gmail.com>
2024-06-25 15:42:10 +0900

    refactor: shorten encryptor method

--- src/main/java/dreamtree/jlog/service/RoomService.java
+++ src/main/java/dreamtree/jlog/service/RoomService.java
@@ -1,5 +1,8 @@
 package dreamtree.jlog.service;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,6 +10,7 @@ import dreamtree.jlog.domain.Member;
 import dreamtree.jlog.domain.Room;
 import dreamtree.jlog.dto.RoomCreateRequest;
 import dreamtree.jlog.dto.RoomJoinRequest;
+import dreamtree.jlog.exception.JLogException;
 import dreamtree.jlog.repository.MemberRepository;
 import dreamtree.jlog.repository.RoomRepository;
 import dreamtree.jlog.service.finder.RoomFinder;
@@ -21,11 +25,7 @@ public class RoomService {
     private final RoomRepository roomRepository;
     private final MemberRepository memberRepository;
 
-    public RoomService(
-            RoomFinder roomFinder,
-            RoomRepository roomRepository,
-            MemberRepository memberRepository
-    ) {
+    public RoomService(RoomFinder roomFinder, RoomRepository roomRepository, MemberRepository memberRepository) {
         this.roomFinder = roomFinder;
         this.roomRepository = roomRepository;
         this.memberRepository = memberRepository;
@@ -33,7 +33,7 @@ public class RoomService {
 
     @Transactional
     public String create(RoomCreateRequest request) {
-        String code = Encryptor.randomUniqueString(ROOM_CODE_LENGTH);
+        String code = randomUniqueString();
         Member savedMember = memberRepository.save(new Member(request.username()));
         Room savedRoom = roomRepository.save(new Room(code, savedMember));
         return savedRoom.code();
@@ -51,5 +51,15 @@ public class RoomService {
         }
         room.join(member);
         roomRepository.save(room);
+    }
+
+    private String randomUniqueString() {
+        try {
+            String uuid = UUID.randomUUID().toString();
+            String sha256 = Encryptor.toSHA256(uuid);
+            return sha256.substring(0, ROOM_CODE_LENGTH);
+        } catch (NoSuchAlgorithmException e) {
+            throw new JLogException("Failed to generate random unique string");
+        }
     }
 }
