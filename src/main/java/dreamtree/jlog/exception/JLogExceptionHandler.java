49833967214c8145959263613e325b7640af1a19
재우 <Akaps6768@gmail.com>
2024-09-22 20:37:11 +0900

    fix: modify username regex

--- src/main/java/dreamtree/jlog/exception/JLogExceptionHandler.java
+++ src/main/java/dreamtree/jlog/exception/JLogExceptionHandler.java
@@ -5,9 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class JLogExceptionHandler {
+public class JLogExceptionHandler extends ResponseEntityExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
