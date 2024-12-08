2a51892abb29d05588153b6089840d8ee2a92ead
재우 <Akaps6768@gmail.com>
2024-12-09 08:52:58 +0900

    remove unused constructor

--- src/main/java/dreamtree/jlog/dto/LogRequest.java
+++ src/main/java/dreamtree/jlog/dto/LogRequest.java
@@ -12,10 +12,6 @@ public record LogRequest(
         Long expense,
         String memo
 ) {
-    public LogRequest(String roomCode, String username) {
-        this(null, roomCode, username, null, null);
-    }
-
     public LogRequest(long id, String roomCode, String username) {
         this(id, roomCode, username, null, null);
     }
