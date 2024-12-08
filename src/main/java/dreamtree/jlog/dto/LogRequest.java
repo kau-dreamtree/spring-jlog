d898780c89ac469b0af28373823bb99b47a629f0
재우 <Akaps6768@gmail.com>
2024-12-09 08:53:16 +0900

    improve dto readability

--- src/main/java/dreamtree/jlog/dto/LogRequest.java
+++ src/main/java/dreamtree/jlog/dto/LogRequest.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record LogRequest(
         @JsonProperty("log_id")
         Long id,
+
         @JsonProperty("room_code")
         String roomCode,
+
         String username,
+
         @JsonProperty("amount")
         Long expense,
+
         String memo
 ) {
     public LogRequest(long id, String roomCode, String username) {
