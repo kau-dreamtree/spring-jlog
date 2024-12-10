116bbd3b9c7c010f7acba2d63b58a5f93a410d7b
재우 <Akaps6768@gmail.com>
2024-12-10 11:32:07 +0900

    have abstract class base entity testable

--- src/main/java/com/jlog/domain/BaseEntity.java
+++ src/main/java/com/jlog/domain/BaseEntity.java
@@ -2,6 +2,7 @@ package com.jlog.domain;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 
@@ -16,9 +17,20 @@ import lombok.Getter;
 @Getter
 public abstract class BaseEntity {
 
+    public BaseEntity() {
+        this(LocalDateTime.now());
+    }
+
+    private BaseEntity(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+        this.modifiedDate = createdDate;
+    }
+
     @CreatedDate
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdDate;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedDate;
 }
