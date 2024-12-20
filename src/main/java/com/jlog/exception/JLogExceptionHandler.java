16fbdedc6edec6fba6ddc68b52ee9361033e0148
Jaewoo Shin <Akaps6768@gmail.com>
2024-12-20 19:12:36 +0900

    fix datetime format (#69)

--- src/main/java/com/jlog/exception/JLogExceptionHandler.java
+++ src/main/java/com/jlog/exception/JLogExceptionHandler.java
@@ -1,20 +1,14 @@
 package com.jlog.exception;
 
-import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.util.WebUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -38,28 +32,12 @@ public class JLogExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<Object> handleExceptionInternal(
-            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
-
-        if (request instanceof ServletWebRequest servletWebRequest) {
-            HttpServletResponse response = servletWebRequest.getResponse();
-            if (response != null && response.isCommitted()) {
-                if (logger.isWarnEnabled()) {
-                    logger.warn("Response already committed. Ignoring: " + ex);
-                }
-                return null;
-            }
-        }
-
-        if (body == null && ex instanceof ErrorResponse errorResponse) {
-            body = errorResponse.updateAndGetBody(getMessageSource(), LocaleContextHolder.getLocale());
-        }
-
-        if (statusCode.equals(HttpStatus.INTERNAL_SERVER_ERROR) && body == null) {
-            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
-        }
-
+            @NonNull Exception ex,
+            @Nullable Object body,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode statusCode,
+            @NonNull WebRequest request) {
         log.error(ex.getMessage(), ex);
-
-        return createResponseEntity(body, headers, statusCode, request);
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
 }
