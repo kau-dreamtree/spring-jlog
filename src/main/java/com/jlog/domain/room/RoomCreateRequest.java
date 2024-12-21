3a6af139b598ab9abc858fed8929d80f871cd6f3
재우 <Akaps6768@gmail.com>
2024-12-21 11:15:50 +0900

    mark `@SuppressWarnings` on deprecated warnings

--- src/main/java/com/jlog/domain/room/RoomCreateRequest.java
+++ src/main/java/com/jlog/domain/room/RoomCreateRequest.java
@@ -3,6 +3,8 @@ package com.jlog.domain.room;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@SuppressWarnings("DeprecatedIsStillUsed")
+@Deprecated(forRemoval = true)
 public record RoomCreateRequest(
         @NotBlank
         @Size(min = 2, max = 16)
