a37dadcde80881a59ead4d4ec23303f62b4ea81f
재우 <Akaps6768@gmail.com>
2024-12-24 10:42:28 +0900

    fix test

--- src/test/java/com/jlog/domain/log/LogRepositoryTest.java
+++ src/test/java/com/jlog/domain/log/LogRepositoryTest.java
@@ -42,15 +42,15 @@ class LogRepositoryTest {
         logRepository.save(log2);
         logRepository.save(log3);
 
-        var pageRequest = PageRequest.of(0, 20, Direction.DESC, "createdAt");
+        var pageRequest = PageRequest.of(0, 20, Direction.DESC, "id");
 
         // when
-        List<Log> actual = logRepository.findLogsByRoomAfterId(room, 1L, pageRequest);
+        List<Log> actual = logRepository.findLogsByRoomAfterId(room, 3L, pageRequest);
 
         // then
         assertThat(actual).hasSize(2);
-        assertThat(actual.get(0).getExpense()).isEqualTo(3000L);
-        assertThat(actual.get(1).getExpense()).isEqualTo(2000L);
+        assertThat(actual.get(0).getExpense()).isEqualTo(2000L);
+        assertThat(actual.get(1).getExpense()).isEqualTo(1000L);
     }
 
     @Test
