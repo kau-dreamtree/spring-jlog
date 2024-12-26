ad2e6015d13e84ccc601e483bc7a7adbbbb386b6
Jaewoo Shin <Akaps6768@gmail.com>
2024-12-26 22:16:52 +0900

    fix datetime format (#89)

--- src/main/java/com/jlog/config/JLogDateTimeDeserializer.java
+++ src/main/java/com/jlog/config/JLogDateTimeDeserializer.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 
 public final class JLogDateTimeDeserializer extends LocalDateTimeDeserializer {
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS+09:00");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss+09:00");
 
     public JLogDateTimeDeserializer() {
         super(DATE_TIME_FORMATTER);
--- src/main/java/com/jlog/config/JLogDateTimeSerializer.java
+++ src/main/java/com/jlog/config/JLogDateTimeSerializer.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 public final class JLogDateTimeSerializer extends LocalDateTimeSerializer {
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS+09:00");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss+09:00");
 
     public JLogDateTimeSerializer() {
         super(DATE_TIME_FORMATTER);
