d8a9802db9d8277afd8acdad524f34bea4638807
재우 <Akaps6768@gmail.com>
2025-01-24 13:49:43 +0900

    replace static factory methods with constructors

--- src/main/java/com/jlog/domain/member/MemberResponse.java
+++ src/main/java/com/jlog/domain/member/MemberResponse.java
@@ -1,11 +1,7 @@
 package com.jlog.domain.member;
 
-public record MemberResponse(
-        String name,
-        long expense
-) implements MemberDto {
-
-    public static MemberResponse from(Member member) {
-        return new MemberResponse(member.getName(), member.getExpense());
+public record MemberResponse(String name, long expense) implements MemberDto {
+    public MemberResponse(Member member) {
+        this(member.getName(), member.getExpense());
     }
 }
